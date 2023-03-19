8bd7a036fd5bd93b718ef6c12a1a0243b892f506
Azim Sofi <89006182+AzimSofi@users.noreply.github.com>
2023-03-19 20:32:15 +0800

    Update Quicksort.py

--- Quicksort.py
+++ Quicksort.py
@@ -1,18 +1,18 @@
-def quicksort_algorithm(array_input):
+def quicksort_algorithm(input_array):
 
-    if len(array_input) <= 1:
-        return array_input
+    if len(input_array) <= 1:
+        return input_array
 
     else:
-        pivot = array_input[0]
-        left = []
-        right = []
+        pivot = input_array[0]
+        left_array = []
+        right_array = []
         
-        for i in range(1, len(array_input)):
-            if array_input[i] < pivot:
-                left.append(array_input[i])
+        for i in range(1, len(input_array)):
+            if input_array[i] < pivot:
+                left_array.append(input_array[i])
 
             else:
-                right.append(array_input[i])
+                right_array.append(input_array[i])
                 
-        return quicksort_algorithm(left) + [pivot] + quicksort_algorithm(right)
+        return quicksort_algorithm(left_array) + [pivot] + quicksort_algorithm(right_array)
